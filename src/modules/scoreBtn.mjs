1e64901f2bf06a99ec39cc8d02b47fde8870c1c3
Retky <jose.arredondo.a@outlook.com>
2022-04-05 16:29:21 -0500

    Add 'formBtn' function
    To listen submit

--- src/modules/scoreBtn.mjs
+++ src/modules/scoreBtn.mjs
@@ -2,3 +2,16 @@ const scoreForm = document.querySelector('#addScore')
 const scoreName = scoreForm.querySelectorAll('input')[0]
 const scoreVal = scoreForm.querySelectorAll('input')[1]
 const scoreBtn = scoreForm.querySelectorAll('input')[2]
+
+const formBtn = () => {
+  scoreForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (scoreName.value !== '' && scoreVal.value >= 0) {
+      console.log(scoreName.value);
+    }
+  });
+}
+
+export default formBtn;
+
+// TODO: Change scoreVal to type text [numbers | > 0]
