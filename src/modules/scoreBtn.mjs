0a881afbd6bf244b113b35396708ef9d7d1f5b46
Retky <jose.arredondo.a@outlook.com>
2022-04-05 17:01:45 -0500

    Add 'scoreBtn' function
    To add scores in html list

--- src/modules/scoreBtn.mjs
+++ src/modules/scoreBtn.mjs
@@ -1,16 +1,19 @@
-const scoreForm = document.querySelector('#addScore')
-const scoreName = scoreForm.querySelectorAll('input')[0]
-const scoreVal = scoreForm.querySelectorAll('input')[1]
-const scoreBtn = scoreForm.querySelectorAll('input')[2]
+import addScore from './addScore.mjs'
+
+const scoreForm = document.querySelector('#addScore');
+const scoreName = scoreForm.querySelectorAll('input')[0];
+const scoreVal = scoreForm.querySelectorAll('input')[1];
+const scoreBtn = scoreForm.querySelectorAll('input')[2];
 
 const formBtn = () => {
   scoreForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (scoreName.value !== '' && scoreVal.value >= 0) {
       console.log(scoreName.value);
-    }
+      addScore(scoreName.value, scoreVal.value)
+    };
   });
-}
+};
 
 export default formBtn;
 
