05c48d5ea70b1903ab34262f0c35e5b65b3f9361
Retky <jose.arredondo.a@outlook.com>
2022-04-05 15:53:45 -0500

    Add 'form' selectors

---
+++ src/modules/scoreBtn.mjs
@@ -0,0 +1,4 @@
+const scoreForm = document.querySelector('#addScore')
+const scoreName = scoreForm.querySelectorAll('input')[0]
+const scoreVal = scoreForm.querySelectorAll('input')[1]
+const scoreBtn = scoreForm.querySelectorAll('input')[2]
