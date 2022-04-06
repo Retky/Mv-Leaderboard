6d48de1acd676c9490f832c8c327d6d7581f4361
Retky <jose.arredondo.a@outlook.com>
2022-04-06 15:30:21 -0500

    Modify 'addScore' fucntion to work with testAPI

--- src/modules/addScore.mjs
+++ src/modules/addScore.mjs
@@ -2,7 +2,13 @@ const scoreHTML = document.querySelector('#scoreList');
 
 const addScore = (name, value) => {
   const scoreElement = document.createElement('li');
-  
+  //THIS URL IS FOR TESTING ONLY
+  fetch('https://apitry-58971-default-rtdb.firebaseio.com/games/-N--QAwimOYIiEsc5i32.json', {
+    method: 'POST', body: JSON.stringify({
+	    "user": name,
+	    "score": value
+    })
+  });
 };
 
 export default addScore;
