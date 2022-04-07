447d585fbfd9f1a61ff46b8eb0f7587f48a9f1db
Retky <jose.arredondo.a@outlook.com>
2022-04-06 20:15:08 -0500

    Modify 'addScore' fn to work with API

--- src/modules/addScore.mjs
+++ src/modules/addScore.mjs
@@ -3,11 +3,15 @@ const scoreHTML = document.querySelector('#scoreList');
 const addScore = (name, value) => {
   const scoreElement = document.createElement('li');
   //THIS URL IS FOR TESTING ONLY
-  fetch('https://apitry-58971-default-rtdb.firebaseio.com/games/-N--QAwimOYIiEsc5i32.json', {
-    method: 'POST', body: JSON.stringify({
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JV1cLaJ1leCS3Qcmz7ZM/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
 	    "user": name,
 	    "score": value
-    })
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    }
   });
 };
 
