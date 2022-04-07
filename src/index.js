1580044fbeecaccffd8744a31aec567ac09bf00a
Retky <jose.arredondo.a@outlook.com>
2022-04-07 12:40:27 -0500

    Run build

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import buildScore from './modules/buildScore.mjs';
-import formBtn from './modules/scoreBtn.mjs';
-import refresh from './modules/refresh.mjs';
-
-buildScore();
-formBtn();
-refresh();
