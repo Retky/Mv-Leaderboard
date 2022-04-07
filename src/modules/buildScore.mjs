7dbb76127bb80f72a7e5e96eef7c7a862643ca14
Retky <jose.arredondo.a@outlook.com>
2022-04-06 20:15:37 -0500

    Modify 'buildScore' to work with API

--- src/modules/buildScore.mjs
+++ src/modules/buildScore.mjs
@@ -1,17 +1,16 @@
-const li = [
-  {name: 'one', score: 23},
-  {name: 'two', score: 35}
-]
-
-const board = document.querySelector('#scoreList');
+import fetching from './scoreAPI.mjs';
 
 const buildScore = () => {
-  li.forEach((item) => {
-    const listElement = document.createElement('li');
+  const board = document.querySelector('#scoreList');
+    const scoreAPI = fetching()
+    .then((board) => {
+      board.forEach((item) => {
+        const listElement = document.createElement('li');
 
-    listElement.innerHTML = `${item.name} : ${item.score}`;
-    scoreList.appendChild(listElement);
-  });
+        listElement.innerHTML = `${item.user} : ${item.score}`;
+        scoreList.appendChild(listElement);
+      });
+    });
 };
 
 export default buildScore;
